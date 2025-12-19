7aab7e3e4bb46fd8335daae4975e9e7c53b31f51
mrz3107 <mirgiyasovmirziyod@gmail.com>
2025-12-19 10:07:31 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-#1.Dictionary yaratish va unga 5 ta element qo‘shish.
-#2.Foydalanuvchidan kalit kiritishini so‘rab, shu kalitga tegishli qiymatni ekranga chiqarish (get()).
-#3.Dictionary dan kalit bo‘yicha elementni o‘chirish (pop()).
-#4.Dictionary ning barcha kalit va qiymatlarini alohida chiqarish (keys(), values()).
-#5.Dictionary ustida for sikli yordamida barcha elementlarni ekranga chiqarish.
 
 Dic = {'Ali': "13 Yosh", "Alex": "15 yosh", 'Asil': "20 Yosh", "Alinur": "16 yosh", "Axmadjon": "19 yosh",}
 print (Dic)
